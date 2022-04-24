75165e2442c7b5d639868aeb63e65be6a7cee8e8
LaelBelchior <laelbelchior@gmail.com>
2022-04-24 19:23:16 -0300

    Iniciando excecao atraves da classe SaldoInsuficienteException e usando no arquivo Conta.php

--- src/Modelo/Conta/Conta.php
+++ src/Modelo/Conta/Conta.php
@@ -2,11 +2,13 @@
 
 namespace Alura\Banco\Modelo\Conta;
 
+use SaldoInsuficiente;
+
 abstract class Conta
 {
-    private $titular;
-    protected $saldo;
-    private static $numeroDeContas = 0;
+    private string $titular;
+    protected float $saldo;
+    private static int $numeroDeContas = 0;
 
     public function __construct(Titular $titular)
     {
@@ -26,8 +28,7 @@ abstract class Conta
         $tarifaSaque = $valorASacar * $this->percentualTarifa();
         $valorSaque = $valorASacar + $tarifaSaque;
         if ($valorSaque > $this->saldo) {
-            echo "Saldo indisponível";
-            return;
+            throw new SaldoInsuficiente();
         }
 
         $this->saldo -= $valorSaque;
