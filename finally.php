94d9be1ae41714265de653e1cc7a6e458294845a
LaelBelchior <laelbelchior@gmail.com>
2022-04-25 20:47:23 -0300

    Dando uma funçao real ao finally

--- finally.php
+++ finally.php
@@ -1,11 +1,21 @@
 <?php
 
-try{
-    echo "Executando bloco try" . PHP_EOL;
-    throw new Exception();
-} catch (Throwable $e){
-    echo "Executando bloco catch" . PHP_EOL;
-    $e -> getMessage();
-} finally{
-    echo "Executando bloco finally" . PHP_EOL;
+//Dando uma aplicação real ao finally. Como o Try e o Catch tem return,
+//se eu precisar colocar alguma coisa depois desse no bloco na função, nunca vai ser lido.
+//Aí entra o finally, p garantir q vai ser lido sem problemas p não interromper pelo return.
+
+function a(): int
+{
+    try{
+        echo "Executando o try" . PHP_EOL;
+        throw new Exception();
+        return 0;
+    } catch(Throwable $e){
+        echo "Executando o catch" . PHP_EOL;
+        return 1;
+    } finally{
+        echo "Final da função" . PHP_EOL;
+    }
 }
+
+a();
