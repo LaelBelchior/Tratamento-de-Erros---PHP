924256b075607209dd21b4c1367355ad400766b1
LaelBelchior <laelbelchior@gmail.com>
2022-04-25 21:02:48 -0300

    Vendo comportamento do PHP com erros

---
+++ erros.php
@@ -0,0 +1,10 @@
+<?php
+
+//PHP para com Warning, mas continua rodando com Notice.
+
+error_reporting(E_ALL); // Serve para exibir todos os erros.
+
+echo $variavel;
+echo $array[12];
+
+echo CONSTANTE;
